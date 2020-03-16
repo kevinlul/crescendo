747c14f2944dc7cad2896ca6a28dcfda688a2409
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-03-16 13:16:25 -0400

    Add support for a webhook callback on container completion

--- crescendo.js
+++ crescendo.js
@@ -1,6 +1,7 @@
 const debug = require("debug");
 const Docker = require("dockerode");
 const express = require("express");
+const fetch = require("node-fetch");
 const fs = require("fs-extra");
 const morgan = require("morgan");
 
@@ -16,6 +17,8 @@ const services = servicesConfig.reduce(
         obj[config.name] = new Service(docker, config),
         obj
     ), {});
+
+fs.mkdirpSync(process.env.LOG_DIR);
 
 const crescendo = express();
 // combined + response time
@@ -37,9 +40,10 @@ function createService(req, res) {
         return;
     }
     const timestamp = Date.now();
-    fs.mkdirpSync(process.env.LOG_DIR);
-    const sout = fs.createWriteStream(`${process.env.LOG_DIR}/${timestamp}.out.log`);
-    const serr = fs.createWriteStream(`${process.env.LOG_DIR}/${timestamp}.err.log`);
+    const outpath = `${process.env.LOG_DIR}/${timestamp}.out.log`;
+    const errpath = `${process.env.LOG_DIR}/${timestamp}.err.log`;
+    const sout = fs.createWriteStream(outpath);
+    const serr = fs.createWriteStream(errpath);
     logger("Log timestamp %d", timestamp);
     try {
         var run = services[req.params.service].run(
@@ -50,16 +54,38 @@ function createService(req, res) {
         );
         logger("Starting %s container with log timestamp %d", req.params.service, timestamp);
         res.status(201).send(timestamp + "");
-        setImmediate(async () => {
-            try {
-                const [runResult] = await run;
-                logger("%d completed: %o", timestamp, runResult);
-            } catch(err) {
-                logger("%d errored: %o", timestamp, err);
-            }
-        });
+        setImmediate(serviceCompleteThunk(run, timestamp, req.body.webhook));
     } catch (err) {
+        sout.end();
+        fs.remove(outpath);
+        serr.end();
+        fs.remove(errpath);
         res.status(400).send(err.message);
+    }
+}
+
+function serviceCompleteThunk(run, timestamp, webhook) {
+    return async () => {
+        let runResult;
+        try {
+            [runResult] = await run;
+            logger("%d completed: %o", timestamp, runResult);
+        } catch (err) {
+            logger("%d errored: %o", timestamp, err);
+            return;
+        }
+        if (webhook) {
+            try {
+                const response = await fetch(webhook, {
+                    method: "POST",
+                    body: JSON.stringify(runResult),
+                    headers: { "Content-Type": "application/json" }
+                });
+                logger("%d post to webhook %s: %d %s", timestamp, webhook, response.status, response.statusText);
+            } catch (err) {
+                logger("%d error on webhook %s: %o", timestamp, webhook, err);
+            }
+        }
     }
 }
 
